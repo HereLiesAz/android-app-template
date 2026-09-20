8aa0960cd93cb7136a83d3a71d2ea2714ff68805
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-19 19:14:20 -0500

    Build starter template

---
+++ app/src/main/java/com/hereliesaz/templateapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.hereliesaz.templateapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { TemplateApp() }
+    }
+}
+
+@Composable
+private fun TemplateApp() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("HereLiesAz Android template")
+            }
+        }
+    }
+}
